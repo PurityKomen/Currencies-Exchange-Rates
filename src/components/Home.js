02a9bc941684145fe35610930e920f0f2eb62954
PurityKomen <jepkorirpurity8@gmail.com>
2024-11-18 09:33:30 +0300

    unzip file and get data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,42 +1,69 @@
-import React, { useEffect, useState }  from 'react'
-import Search from './Search'
-import CurrenciesList from './CurrenciesList'
+import React, { useEffect, useState } from "react";
+import Search from "./Search";
+import CurrenciesList from "./CurrenciesList";
+import JSZip from "jszip";
 
 function Home() {
-  const [currencies,setCurrencies] = useState([]) 
+  const [currencies, setCurrencies] = useState([]);
 
-  useEffect( () => {
+  useEffect(() => {
     fetch("http://localhost:3000/fx")
-    .then(response => response.json())
-    .then(data => setCurrencies(data)) 
-  },[]) 
-  
+      .then((response) => response.json())
+      .then((data) => setCurrencies(data));
+  }, []);
+
+  const unzip = async () => {
+    const zip = new JSZip();
+    try {
+      const zipData = await fetch("../../flags.zip");
+
+      // Convert the Response to Blob
+      const blob = await zipData.blob();
+
+      await zip.loadAsync(blob).then((zip) => {
+        console.log("data2", zip.file.length);
+        return zip.file("text/html").async("string");
+      });
+    } catch (error) {
+      console.error("Error loading ZIP file:", error);
+
+      // Handle the error gracefully, such as displaying a message to the user
+    }
+  }; 
+  console.log("data3", unzip());
+
 
   return (
-    <div className='container m-3'>
-        <h3>Currencies and Exchange Rate</h3>
-        <Search />
+    <div className="container m-3">
+      <h3>Currencies and Exchange Rate</h3>
+      <Search />
 
-        <table className="table table-bordered table-striped">
-          <thead>
-            <tr>
-              <th scope="col">Flag</th>
-              <th scope="col">Name</th>
-              <th scope="col">Currency</th>
-              <th scope="col">Exchange Rate</th>
-            </tr>
-          </thead>
+      <table className="table table-bordered table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Flag</th>
+            <th scope="col">Name</th>
+            <th scope="col">Currency</th>
+            <th scope="col">Exchange Rate</th>
+          </tr>
+        </thead>
 
-          <tbody>
-              {currencies.map((item)=>{
-                return <CurrenciesList key={item.id} flag={item.flag} name={item.nameI18N} 
-                currency={item.currency}  exchangeRate={item.exchangeRate?.buy} />
-              })}
-          </tbody>
-        </table>
-
+        <tbody>
+          {currencies.map((item) => {
+            return (
+              <CurrenciesList
+                key={item.id}
+                flag={item.flag}
+                name={item.nameI18N}
+                currency={item.currency}
+                exchangeRate={item.exchangeRate?.buy}
+              />
+            );
+          })}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
