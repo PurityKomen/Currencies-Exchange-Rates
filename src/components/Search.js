b8eda7e5b4e57706149ba0b2e28b1ad6fd60387d
PurityKomen <jepkorirpurity8@gmail.com>
2024-11-19 09:03:51 +0300

    added search term to url

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-function Search({search}) {
-  const [searchTerm, setSearchTerm] = useState('');
+function Search({ search }) {
+  const [searchTerm, setSearchTerm] = useState("");
 
   //set search term to be the value the user inputs on the search input
   const handleChange = (event) => {
@@ -9,25 +9,33 @@ function Search({search}) {
   };
 
   //handle data after a submit
-  function handleSubmit(event){
-    event.preventDefault()
-    search(searchTerm)
-    setSearchTerm("")
-}
+  function handleSubmit(event) {
+    event.preventDefault();
+    search(searchTerm);
+    window.location.hash = `#search=${searchTerm}`;
+    setSearchTerm("");
+  }
 
   return (
-    <div className='d-flex justify-content-center m-4'>
-        <form className="justify-content-center w-50" onSubmit={handleSubmit}>
+    <div className="d-flex justify-content-center m-4">
+      <form className="justify-content-center w-50" onSubmit={handleSubmit}>
         <div className="form-group row">
           <div className="col">
-            <input type="currencies"  className="form-control" placeholder="Search"  value={searchTerm}
-            onChange={handleChange} />
+            <input
+              type="currencies"
+              className="form-control"
+              placeholder="Search"
+              value={searchTerm}
+              onChange={handleChange}
+            />
           </div>
         </div>
-        <button type="submit" className="btn btn-warning m-2">Search</button>
+        <button type="submit" className="btn btn-warning m-2">
+          Search
+        </button>
       </form>
     </div>
-  )
+  );
 }
 
-export default Search
+export default Search;
